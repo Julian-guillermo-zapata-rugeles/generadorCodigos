91b55531c588313ed84cad38599e61354c8a83c4
julian guillermo zapata rugeles <julianruggeles@gmail.com>
2021-09-10 10:55:51 -0500

    se añade la posibilidad de entrada mediante el programa y se conserva el paso de argv

--- main.cpp
+++ main.cpp
@@ -3,13 +3,27 @@
 
 using namespace std;
 
+
+inline void ejecutarGenerador(int &test_number){
+    generador gen;
+    gen.obtener(test_number);
+}
+
+
 int main(int argc, char **argv)
 {
-    if (argc==2) {
-        int x = (int)*argv[1] - 48;
-        generador gen;
-        gen.obtener(x);
+    int test_number;
+    if (argc == 1) {
+        cout << "Ingrese numero (1-9) : ";
+        cin >> test_number;
+        ejecutarGenerador(test_number);
+
+    }
+    else if (argc==2) {
+        test_number = (int)*argv[1] - 48;
+        ejecutarGenerador(test_number);
      }
+
 
     else{
         cout << "Autor: Julián Guillermo Zapata Rugeles "<< endl;
